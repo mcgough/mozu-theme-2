77e18f913543f33eeefc939c5260becc9f647c62
Dan McGough <mcgough.dan@gmail.com>
2016-02-14 18:51:12 -0800

    select changes on table click but propagation bug

--- scripts/modules/product-detail.js
+++ scripts/modules/product-detail.js
@@ -1,22 +1,20 @@
 define(['modules/jquery-mozu'], function($) {
 
-  var options = $('.mz-productoptions-option option');
-  var $rows = $('.mz-productdetail-fulldesc tbody tr');
-  var $prodCode = $('.mz-productcodes-productcode');
-  var $sku;
+  var sku;
   var isActive;
+  var isSelected;
 
-  $(document).on('click', '.mz-productdetail-fulldesc tbody tr', function() {
+  $(document).on('click', '.mz-productdetail-fulldesc tbody tr', function(e) {
+    e.stopPropagation();
+    sku = $(this).children().eq(0).text();
+    $('[sku="' + sku + '"]').prop("selected",true).change();
     isActive = $('tr.active');
     if(isActive.length > 0) {
       isActive.removeClass('active');
     }
     $(this).addClass('active');
-    $sku = ($(this).children().eq(0).text());
-    console.log($sku);
-    // $prodCode.text($sku);
+    return false;
   });
 
-  console.log(options);
-
 });
+
